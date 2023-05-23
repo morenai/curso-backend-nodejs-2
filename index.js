0d333f72bb6857c31f0c418084006a70020ec79b
morenai <miguel.a.castle@gmail.com>
2023-05-23 23:29:59 +0200

    serverRouteUpdate

--- index.js
+++ index.js
@@ -4,7 +4,22 @@ const port = 3000;
 
 app.get('/', (req, res ) => {
   res.send('Hola mi server en express');
+});
+app.get('/nueva-ruta', (req, res ) => {
+  res.send('Hola soy una nueva ruta');
+});
+app.get('/productos', (req, res ) => {
+  res.json({
+    name: 'Product 1',
+    price: '1000'
+  });
+app.get('/home', (req, res ) => {
+  res.json({
+    name: 'Home',
+    price: '5000'
+  });
 })
+});
 
 app.listen(port, () => {
   console.log('mi port ' +  port);
